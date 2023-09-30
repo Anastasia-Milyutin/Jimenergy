a674a3633c8d156da5ebbd6a47b66cf13f657b20
Anastasia-Milyutin <141128897+Anastasia-Milyutin@users.noreply.github.com>
2023-09-30 17:51:00 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-document.querySelectorAll('.connection_btn ').forEach(function(btn) {
-    btn.addEventListener('click', function() {
-      var blockOpen = document.querySelector('.block-open');
-      blockOpen.style.display = 'block';
-    });
+document.querySelectorAll(".connection_btn ").forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    var blockOpen = document.querySelector(".block-open");
+    blockOpen.style.display = "block";
   });
-  document.querySelector('.open-btn-close').addEventListener('click', function() {
-    var blockOpen = document.querySelector('.block-open');
-    blockOpen.style.display = 'none';
-  });
+});
+
+var menuIcon = document.querySelector(".menu-icon");
+var headerMenu = document.querySelector(".header__menu-active");
+
+function toggleMenu() {
+  if (headerMenu.style.display === "none") {
+    headerMenu.style.display = "block";
+  } else {
+    headerMenu.style.display = "none";
+  }
+}
+
+menuIcon.addEventListener("click", toggleMenu);
